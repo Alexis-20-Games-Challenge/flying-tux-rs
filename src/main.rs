cdf9141e3fbcaefc3357a610ba04549aa6e8190b
Alexis Williams <Thecomixguy@hotmail.com>
2024-03-26 13:40:08 +0000

    Begin work on programming

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::path::Path;
+
+// Asset paths
+const BG: &str = "./src/assets/bg.png";
+const TUX: &str = "./src/assets/tux.png";
+const BSOD: &str = "./src/assets/bsod.png";
+
+// Dimensional Constants
+const WIDTH: u32 = 512;
+const HEIGHT: u32 = 1024;
+
+fn main() {
+    println!("Hello, world!");
+}
